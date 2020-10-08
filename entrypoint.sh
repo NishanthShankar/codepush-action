a9951a4565a52605b3af729f14522ed4562f3740
Jacob Turner <j.israel.turner@gmail.com>
2020-10-08 14:55:11 -0600

    Put directory in proper file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+if [[ -z "${INPUT_directory}" ]]; then
+  # No input directory provided
+  cd "$(dirname "$0")/.."
+else
+  cd "${INPUT_directory}"
+fi
+
 if [ -z "$APPCENTER_ACCESS_TOKEN" ]; then
     echo "APPCENTER_ACCESS_TOKEN is required to run commands with the appcenter cli"
     exit 126
