9db5abf2a647ca5bbc15aec0d5036724a30128f4
Jacob Turner <j.israel.turner@gmail.com>
2020-10-08 15:31:53 -0600

    Debug input directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,17 @@
 set -e
 
 if [ -z "${INPUT_directory}" ]; then
+  echo "No input directory provided"
   # No input directory provided
   cd "$(dirname "$0")/.."
 else
+  echo "Input directory provided:"
+  echo "${INPUT_directory}"
   cd "${INPUT_directory}"
 fi
+
+echo "PWD:"
+echo $PWD
 
 if [ -z "$APPCENTER_ACCESS_TOKEN" ]; then
     echo "APPCENTER_ACCESS_TOKEN is required to run commands with the appcenter cli"
